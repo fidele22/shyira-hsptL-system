1da54743b8d43eab1dcaca68ec2faf64cbc2b95c
fidele22 <fidelehirwa2@gmail.com>
2024-08-26 13:13:00 +0200

    db config update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/shyiradb', { 
+    await mongoose.connect('mongodb+srv://fidelehirwa23:K9Z0AKzw5EHhJ6nH@cluster0.pcjespr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', { 
       // You can add options here if needed
     });
     console.log('MongoDB connected');
