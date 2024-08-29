48fcbd3ea35e19073d118ae95e628982a7fe3ad2
fidele22 <fidelehirwa2@gmail.com>
2024-08-29 09:58:57 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error('Not allowed by CORS found'));
     }
   },
 };
